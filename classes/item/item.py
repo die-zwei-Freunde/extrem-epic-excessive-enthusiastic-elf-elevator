1e232c91411a7aab8c018404912c6f52ca522bb2
jan-menno <janmenno@yahoo.de>
2021-06-11 16:37:56 +0200

    introduced item base class

---
+++ classes/item/item.py
@@ -0,0 +1,14 @@
+class Item():
+    '''Abstract Base class for any items; equippable and useable.'''
+    def __init__(self, id, race, alignment, ):
+        self.id = id
+        self.race = race
+        self.align = alignment
+
+    def get_possible_races(self):
+        '''Get the list of all possible races that may use/equip this item.'''
+        return self.race
+
+    def get_possible_alignments(self):
+        '''Get the list of all possible alignments that may use/equip this item.'''
+        return self.align
